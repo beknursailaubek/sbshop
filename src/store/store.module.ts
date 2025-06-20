65e3e080b82d17f34cc3bac74da64920539b0e67
beknursailaubek <b3knursailaub3k@gmail.com>
2025-06-20 15:52:35 +0500

    feat: implement CRUD operations for store module

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { StoreService } from './store.service';
-import { StoreController } from './store.controller';
+import { Module } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { StoreController } from './store.controller'
+import { StoreService } from './store.service'
 
 @Module({
-  controllers: [StoreController],
-  providers: [StoreService],
+	controllers: [StoreController],
+	providers: [StoreService, PrismaService]
 })
 export class StoreModule {}
