69ec712e07457915134b83fc13de7e8781f4c149
beknursailaubek <b3knursailaub3k@gmail.com>
2025-06-26 12:42:52 +0500

    feat: complete backend implementation

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
-import { OrderService } from './order.service';
+import {
+	Body,
+	Controller,
+	HttpCode,
+	Post,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { CurrentUser } from 'src/user/decorators/user.decorator'
+import { OrderDto } from './dto/order.dto'
+import { PaymentStatusDto } from './dto/payment-status.dto'
+import { OrderService } from './order.service'
 
-@Controller('order')
+@Controller('orders')
 export class OrderController {
-  constructor(private readonly orderService: OrderService) {}
+	constructor(private readonly orderService: OrderService) {}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('place')
+	@Auth()
+	async checkout(@Body() dto: OrderDto, @CurrentUser('id') userId: string) {
+		return this.orderService.createPayment(dto, userId)
+	}
+
+	@HttpCode(200)
+	@Post('status')
+	async updateStatus(@Body() dto: PaymentStatusDto) {
+		return this.orderService.updateStatus(dto)
+	}
 }
